27774c31e85fc661d44085b3636bd11b5e057492
Antonio Valencia <eumana@bitworks.com.sv>
2025-06-28 09:42:28 -0600

    create user routes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const usersRouter = require("./src/users/routes/users.routes");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/users", usersRouter);
 
 app.listen(port, "192.168.1.75", () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/users/routes/users.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+//const userController = require('../controllers/users.controller');
+
+//router.post("/sign-up", userController);
+//router.post("/login", userController);
+
+module.exports = router;
