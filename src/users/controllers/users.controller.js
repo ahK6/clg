6b21909b249eec7bed3e3cc0a04cf490921835e9
Antonio Valencia <eumana@bitworks.com.sv>
2025-06-28 10:17:39 -0600

    login endpoint

--- src/users/controllers/users.controller.js
+++ src/users/controllers/users.controller.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require("../../../prisma/prisma");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res) => {
   const prisma = new PrismaClient();
@@ -17,6 +18,36 @@ exports.signUp = async (req, res) => {
     res.status(201).json({ message: "User created successfully", user });
   } catch (error) {
     res.status(500).json({ error: "Error creating user" });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+exports.login = async (req, res) => {
+  const prisma = new PrismaClient();
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error logging in" });
   } finally {
     await prisma.$disconnect();
   }
--- src/users/routes/users.routes.js
+++ src/users/routes/users.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const userController = require("../controllers/users.controller");
 
 router.post("/sign-up", userController.signUp);
-//router.post("/login", userController);
+router.post("/login", userController.login);
 
 module.exports = router;
