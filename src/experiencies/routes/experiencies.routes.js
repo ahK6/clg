f40acfe2ec065cb25f11da4a255dfa00179b8e80
Antonio Valencia <eumana@bitworks.com.sv>
2025-06-28 11:46:36 -0600

    experiences validator

--- src/experiencies/routes/experiencies.routes.js
+++ src/experiencies/routes/experiencies.routes.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const experienciesController = require("../controllers/experiencies.controller");
+const {
+  experiencesValidation,
+} = require("../validators/experiencies.validators");
 
 const jwtValidator = require("../../global/middlewares/jwt_validator");
 
 router.post(
   "/create-experience",
   jwtValidator.verifyToken,
+  experiencesValidation,
   experienciesController.createExperiencie
 );
 
---
+++ src/experiencies/validators/experiencies.validators.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+
+const experienciesSchemaValidation = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": "Nombre es requerido",
+  }),
+  dates: Joi.array()
+    .items(
+      Joi.object({
+        date: Joi.string().required(),
+        availables: Joi.number().greater(0).required(),
+      })
+    )
+    .min(1)
+    .required(),
+});
+
+exports.experiencesValidation = (req, res, next) => {
+  const { error } = experienciesSchemaValidation.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
